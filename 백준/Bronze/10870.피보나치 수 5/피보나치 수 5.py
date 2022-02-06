a7ac1a547f44260d2a3fdff8a4a059a73891e79f
Yujin <84908849+ujina123@users.noreply.github.com>
2022-02-06 11:37:45 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/10870.피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870.피보나치 수 5/피보나치 수 5.py
@@ -1,6 +1,6 @@
 import sys
 
-def pibo(n):
+def fibo(n):
     dic[str(n)]=dic.get(str(n-1)) + dic.get(str(n-2)) 
 
 N = int(sys.stdin.readline())
@@ -8,6 +8,6 @@ N = int(sys.stdin.readline())
 dic = {'0':0, '1':1}
 
 for i in range(2,N+1):
-    pibo(i)
+    fibo(i)
 
 print(dic.get(f'{str(N)}'))
