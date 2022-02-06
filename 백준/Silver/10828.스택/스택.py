bdb1c20cc392b1e92370f33c021dd477cab5f61b
Yujin <84908849+ujina123@users.noreply.github.com>
2022-02-06 11:20:47 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/10828.스택/스택.py
@@ -0,0 +1,43 @@
+import sys
+
+def push(num):
+    res.append(int(num))
+    return res
+
+def pop():
+    if len(res) == 0:
+        print(-1)
+    else: 
+        print(res[-1])
+        res.pop()
+        
+def size():
+    print(len(res))
+
+def empty():
+    if len(res) == 0:
+        print(1)
+    else:
+        print(0)
+
+def top():
+    if len(res) == 0:
+        print(-1)
+    else: 
+        print(res[-1])
+
+
+N = int(sys.stdin.readline())
+res = []
+for _ in range(N):
+    func = sys.stdin.readline().strip()
+    if func.find('push') == 0:
+        push(func[4:])
+    elif func == 'pop':
+        pop()
+    elif func == 'top':
+        top()
+    elif func == 'size':
+        size()
+    elif func == 'empty':
+        empty()
